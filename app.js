d633491342679792e8f6f3ff096ee9b3e1a5cb13
Zineb H <93009591+Zineb06@users.noreply.github.com>
2023-06-05 21:00:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,60 +8,26 @@
   let dinoFrame
   let currentFrameTime
 
-  export function setupDino() {
+function setupDino() {
     isJumping = false
     dinoFrame = 0
     currentFrameTime = 0
-    yVelocity = 0
     setCustomProperty(dinoElem, "--bottom", 0)
     document.removeEventListener("keydown", onJump)
     document.addEventListener("keydown", onJump)
   }
   
-  export function updateDino(delta, speedScale) {
+function updateDino(delta, speedScale) {
     handleRun(delta, speedScale)
     handleJump(delta)
   }
   
-  export function getDinoRect() {
+function getDinoRect() {
     return dinoElem.getBoundingClientRect()
   }
   
-  export function setDinoLose() {
-    dinoElem.src = "imgs/lose.png"
-  }
-  
-  function handleRun(delta, speedScale) {
-    if (isJumping) {
-      dinoElem.src = `imgs/standing.png`
-      return
-    }
-  
-    if (currentFrameTime >= FRAME_TIME) {
-      dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
-      dinoElem.src = `imgs/run-${dinoFrame}.png`
-      currentFrameTime -= FRAME_TIME
-    }
-    currentFrameTime += delta * speedScale
-  }
-  
-  function handleJump(delta) {
-    if (!isJumping) return
-  
-    incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta)
-  
-    if (getCustomProperty(dinoElem, "--bottom") <= 0) {
-      setCustomProperty(dinoElem, "--bottom", 0)
-      isJumping = false
-    }
-  
-    yVelocity -= GRAVITY * delta
-  }
-  
-  function onJump(e) {
     if (e.code !== "Space" || isJumping) return
-  
-    yVelocity = JUMP_SPEED
+
     isJumping = true
   }
   
